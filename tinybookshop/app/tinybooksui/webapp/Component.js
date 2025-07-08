119fead71b51e1c87d46abdf5d70f5bcb2359e8f
Lisa Nebel <lisa.nebel@sap.com>
2025-07-08 20:38:25 +0000

    Getting started

---
+++ tinybookshop/app/tinybooksui/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/fe/core/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("my.tinybooksui.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
